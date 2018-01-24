1092daca2adc9d29bb56a5bb395d268fc9ff8d06
Adam Patterson <hello@adampatterson.ca>
2018-01-24 00:31:35 -0700

    Temp Inventory template.

--- templates/template-inventory.php
+++ templates/template-inventory.php
@@ -1,3 +1,26 @@
 <?php
+$data         = get_fields();
 $stock_number = get_query_var('vehicle_id', 0);
-var_dump('Works, This vehicle has the ID of ' . $stock_number);
+
+include(get_template_part_acf('templates/partials/header'));
+
+echo '<!-- template: index/no_posts -->'; ?>
+
+    <div class="wrapper">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <section id="post-404">
+                        <article>
+                            <h1>Works!</h1>
+                            <p><?= 'This vehicle has the ID of ' . $stock_number; ?></p>
+                        </article>
+                    </section>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+<?php include(get_template_part_acf('templates/partials/footer'));
