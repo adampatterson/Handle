04772c0a2a910f4b11f5146dd8dc0f9233030f15
Adam Patterson <hello@adampatterson.ca>
2019-05-12 22:10:05 -0600

    adampatterson/Axe#23 OOp child theme, cleanup of core theme

--- functions.php
+++ functions.php
@@ -1,29 +1,51 @@
 <?php
+//require_once('lib/init.php');
+//require_once('lib/theme-helpers.php');
+
+//require_once('lib/theme-api.php');
+//require_once('lib/theme-rewrite.php');
+
+
 // For composer dependencies
-// require 'vendor/autoload.php';
+require_once get_template_directory() . '/vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
-require_once('lib/init.php');
-require_once('lib/custom.php');
-require_once('lib/theme-helpers.php');
-require_once('lib/theme-media.php');
+class Admin extends \Axe\Setup\Admin
+{
 
-require_once('lib/theme-api.php');
-require_once('lib/theme-rewrite.php');
-
-
-add_filter('acf/settings/load_json', function($paths) {
-    $paths = array(get_template_directory() . '/acf-json');
-
-    if(is_child_theme()){
-        $paths = array(
-            get_stylesheet_directory() . '/acf-json',
-            get_template_directory() . '/acf-json'
-        );
-
+    public function __construct()
+    {
+        parent::__construct();
     }
 
-    return $paths;
-});
+    public function custom_login_logo()
+    {
+        echo '<style type="text/css">
+        h1 a { background-image: url(' . get_stylesheet_directory_uri() . '/assets/img/adminlogo.png) !important; height: auto;}
+        body.login{ background: #fff; }
+        </style>';
+    }
+}
+
+new Admin;
+
+
+
+
+
+//add_filter('acf/settings/load_json', function($paths) {
+//    $paths = array(get_template_directory() . '/acf-json');
+//
+//    if(is_child_theme()){
+//        $paths = array(
+//            get_stylesheet_directory() . '/acf-json',
+//            get_template_directory() . '/acf-json'
+//        );
+//
+//    }
+//
+//    return $paths;
+//});
 
 /*
  * Used to clear the permalinks so that the custom rewrite rules will work.
---
+++ lib/Custom/Custom.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Handle\Custom;
+
+class Custom
+{
+
+}
