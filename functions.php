aa705a6d1b4d801aa51284b898634c4975952c72
Adam Patterson <hello@adampatterson.ca>
2019-06-02 22:47:27 -0600

    Cleanup

--- functions.php
+++ functions.php
@@ -1,7 +1,4 @@
 <?php
-//require_once('lib/init.php');
-//require_once('lib/theme-helpers.php');
-
 // For composer dependencies
 require_once get_template_directory() . '/vendor/autoload.php';
 require_once 'vendor/autoload.php';
@@ -9,18 +6,3 @@ require_once 'vendor/autoload.php';
 if (class_exists('Handle\\Init')) :
     Handle\Init::register_services();
 endif;
-
-
-//add_filter('acf/settings/load_json', function($paths) {
-//    $paths = array(get_template_directory() . '/acf-json');
-//
-//    if(is_child_theme()){
-//        $paths = array(
-//            get_stylesheet_directory() . '/acf-json',
-//            get_template_directory() . '/acf-json'
-//        );
-//
-//    }
-//
-//    return $paths;
-//});
