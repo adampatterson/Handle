ff4e946fc4bbee9f297a86d86c5873cbba412052
Adam Patterson <hello@adampatterson.ca>
2019-06-03 08:00:43 -0600

    .

--- lib/Custom/Custom.php
+++ lib/Custom/Custom.php
@@ -7,10 +7,10 @@ class Custom
 
     public function __construct()
     {
-        add_action('acf/init', [$this, 'my_acf_init']);
+        add_action('acf/init', [$this, 'acf_init']);
     }
 
-    public function my_acf_init()
+    public function acf_init()
     {
 //        acf_update_setting('google_api_key', '{key}');
     }
--- lib/Helpers.php
+++ lib/Helpers.php
@@ -1,5 +1,4 @@
 <?php
-
 // Uncommenting this would require all assets to be loaded from the Child theme.
 // It will also update the path to all of the parent asset helper functions.
 /*
