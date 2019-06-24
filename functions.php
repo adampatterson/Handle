3d67f6a95bdf429f5c1e4f301193a2483016d00d
Adam Patterson <hello@adampatterson.ca>
2019-06-23 18:02:57 -0600

    .

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+// Allows for extending functions in the parent theme.
 require_once('lib/Helpers.php');
 
 // For composer dependencies
@@ -8,17 +9,3 @@ require_once 'vendor/autoload.php';
 if (class_exists('Handle\\Init')) :
     Handle\Init::register_services();
 endif;
-
-//add_filter('acf/settings/load_json', function($paths) {
-//    $paths = array(get_template_directory() . '/acf-json');
-//
-//    if(is_child_theme()){
-//        $paths = array(
-//            get_stylesheet_directory() . '/acf-json',
-//            get_template_directory() . '/acf-json'
-//        );
-//
-//    }
-//
-//    return $paths;
-//});
