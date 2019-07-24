535208a799c116f765cc25e515ecc7328fd34110
Adam Patterson <hello@adampatterson.ca>
2019-07-23 21:07:16 -0600

    Named media sizes

--- lib/Core/Media.php
+++ lib/Core/Media.php
@@ -7,7 +7,21 @@ class Media
 
     public function register()
     {
-        // Add Image Sizes
+        add_image_size('post_movies', 400, 225, true);
+
+        if (is_admin()) {
+            // Add image sizes to the editor
+            add_filter('image_size_names_choose', array($this, 'add_image_sizes_editor'));
+        }
     }
 
+    public function add_image_sizes_editor($sizes)
+    {
+        return array_merge($sizes, array(
+            'featured' => __('Featured'),
+            'gallery'  => __('Gallery'),
+            'grid'     => __('Grid'),
+            'Square'   => __('Square'),
+        ));
+    }
 }
