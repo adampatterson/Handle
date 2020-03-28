e5f7816919bd3f6c49e5e15b33e3e63bfd3b141e
Adam Patterson <hello@adampatterson.ca>
2020-03-28 13:42:34 -0600

    Moved default Media to Parent theme

--- lib/Core/Media.php
+++ lib/Core/Media.php
@@ -2,23 +2,25 @@
 
 namespace Handle\Core;
 
-class Media
+class Media extends \Axe\Core\Media
 {
+
     public function register()
     {
+        parent::__construct();
+
+        add_image_size('background', 1600, 900, true);
+
         if (is_admin()) {
             // Add image sizes to the editor
-            add_filter('image_size_names_choose', array($this, 'add_image_sizes_editor'));
+            add_filter('image_size_names_choose', [$this, 'add_image_sizes_editor']);
         }
     }
 
     public function add_image_sizes_editor($sizes)
     {
-        return array_merge($sizes, array(
-            'featured' => __('Featured'),
-            'gallery'  => __('Gallery'),
-            'grid'     => __('Grid'),
-            'square'   => __('Square'),
-        ));
+        return array_merge($sizes, [
+            'background' => __('Background'),
+        ]);
     }
 }
