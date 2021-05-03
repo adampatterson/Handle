975c83571235f91743adcd3ca713d1ace788b3a1
Adam Patterson <hello@adampatterson.ca>
2021-05-03 10:01:48 -0600

    .

--- functions.php
+++ functions.php
@@ -3,9 +3,10 @@
 require_once('lib/Helpers.php');
 
 // For composer dependencies
-require_once get_template_directory() . '/vendor/autoload.php';
+require_once get_template_directory().'/vendor/autoload.php';
 require_once 'vendor/autoload.php';
 
 if (class_exists('Handle\\Init')) :
     Handle\Init::register_services();
 endif;
+er('movies_to_record', 'algolia_movie_to_record');
