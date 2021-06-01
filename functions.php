dfb38a0fdb4ee0195645de240b1cbc4a5d2f6939
Adam Patterson <adam@bubbleupmarketing.ca>
2021-06-01 14:48:47 -0600

    .

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php
+
 // Allows for extending functions in the parent theme.
 require_once('lib/Helpers.php');
 
 // For composer dependencies
-require_once get_template_directory() . '/vendor/autoload.php';
+require_once get_template_directory().'/vendor/autoload.php';
 require_once 'vendor/autoload.php';
 
 if (class_exists('Handle\\Init')) :
