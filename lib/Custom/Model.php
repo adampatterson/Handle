61b58a296b89f90d0f0623fe5a4e92802850da20
Adam Patterson <hello@adampatterson.ca>
2021-09-11 12:52:51 -0600

    Example featured news

--- lib/Custom/Model.php
+++ lib/Custom/Model.php
@@ -4,5 +4,15 @@ namespace Handle\Custom;
 
 class Model extends \Axe\Core\Model
 {
-
+/*
+   static function getFeaturedNews()
+    {
+        return new \WP_Query([
+            'posts_per_page' => 4,
+            'orderby'        => 'date',
+            'order'          => 'DESC',
+            'category_name'  => 'featured'
+        ]);
+    }
+ */
 }
