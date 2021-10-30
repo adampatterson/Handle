d002d27a940bfad68a6d20081624faf9ad575e88
Adam Patterson <hello@adampatterson.ca>
2021-10-30 13:33:54 -0600

    Pass use parent to __t()

--- lib/Helpers.php
+++ lib/Helpers.php
@@ -2,7 +2,12 @@
 // Uncommenting this would require all assets to be loaded from the Child theme.
 // It will also update the path to all of the parent asset helper functions.
 /*
-function __t() {
-	return get_stylesheet_directory_uri() . '/';
+function __t($useParent = false)
+{
+    if ($useParent) {
+        return get_template_directory_uri() . '/';
+    }
+
+    return get_stylesheet_directory_uri() . '/';
 }
 */
