74ff0c91f9361a6b1fd7f8c9ac9b25835cbfc3ae
Adam Patterson <hello@adampatterson.ca>
2023-02-02 18:56:47 -0700

    updted webpack and npm package

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,17 +6,6 @@ let scssOptions = {
     processCssUrls: false
 }
 
-let bundles = {
-    'all': [
-        './src/js/app.js'
-    ]
-}
-
-let extractLibs = [
-    'jquery',
-    'bootstrap',
-]
-
 const purgecssWordpress = {
     whitelist: [
         'rtl',
@@ -90,39 +79,31 @@ const purgecssWordpress = {
     ],
 }
 
-mix
-    .setPublicPath('./assets')
-    .sass('src/scss/base.scss', 'css').options(scssOptions)
-    // Extract libraries requires ECMAScript 6 imports in your code.
-    .js(bundles.all, 'js/app.js')
-    .extract(extractLibs)
+mix.setResourceRoot('../../')
+   .setPublicPath('./')
+   .sass( 'src/scss/style.scss', '/' )
+   .sass( 'src/scss/theme.scss', 'assets/css' )
+   .options( scssOptions )
 
-    .purgeCss(
-        {
-            enabled: mix.inProduction(),
-            paths: [
-                /*
-                @todo: load parent theme templates
-                This might be an issue as a lot of the templates are
-                in the parent theme
-                 */
-                path.join(__dirname, '*.php'),
-                path.join(__dirname, 'templates/**/*.php'),
-                path.join(__dirname, '/assets/js/**/*.js'),
-            ],
-            extensions: ['html', 'js', 'php'],
+mix.version()
 
-            // Other options are passed through to Purgecss
-            whitelist: purgecssWordpress.whitelist,
-            whitelistPatterns: purgecssWordpress.whitelistPatterns,
-        }
-    )
+mix.purgeCss(
+    {
+//           enabled: mix.inProduction(),
+        enabled: false,
+        paths: () => [
+            path.join(__dirname, '*.php'),
+            path.join(__dirname, 'templates/**/*.php'),
+            path.join(__dirname, 'woocommerce/**/*.php'),
+            path.join(__dirname, 'assets/js/**/*.js'),
+        ],
+        extensions: ['html', 'js', 'php'],
 
-    .autoload({
-        'jquery': ['$', 'window.jQuery', 'jQuery']
-    })
-
-    .version()
+        // Other options are passed through to Purgecss
+        whitelist: purgecssWordpress.whitelist,
+        whitelistPatterns: purgecssWordpress.whitelistPatterns,
+    }
+)
 
 
 // Production
@@ -130,7 +111,7 @@ if (mix.inProduction()) {
     mix.options({
         terser: {
             terserOptions: {
-                warnings: true
+                warnings: false
             }
         }
     })
@@ -141,25 +122,3 @@ if (mix.inProduction()) {
 
     mix.sourceMaps()
 }
-
-// BrowserSync and LiveReload on `npm run watch` command
-//mix.browserSync({
-//    proxy: 'http://wordpress.local',
-//    port: 8080,
-//    If you want to use HTTPS, update the proxy path
-//    and add tour local certificate paths
-//    https: {
-//        key: '/your/certificates/location/your-local-domain.key',
-//        cert: '/your/certificates/location/your-local-domain.crt'
-//    },
-//    files: [
-//        '**/*.php',
-//        'assets/css/**/*.css',
-//        'assets/js/**/*.js',
-//        '!node_modules',
-//        '!vendor',
-//    ],
-//    ghostMode: false,
-//    open: false,
-//    injectChanges: true,
-//})
