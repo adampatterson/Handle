0c6118be6dd4d8b3e435e6a20fccb64370fe7bd5
Adam Patterson <hello@adampatterson.ca>
2023-10-22 00:56:38 -0600

    update bootstrap and mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,5 @@
 let mix = require('laravel-mix')
 
-require('laravel-mix-purgecss')
-
 let scssOptions = {
     processCssUrls: false
 }
@@ -17,106 +15,12 @@ let extractLibs = [
     'bootstrap',
 ]
 
-const purgecssWordpress = {
-    whitelist: [
-        'rtl',
-        'home',
-        'blog',
-        'archive',
-        'date',
-        'error404',
-        'logged-in',
-        'admin-bar',
-        'no-customize-support',
-        'custom-background',
-        'wp-custom-logo',
-        'pagination',
-        'alignnone',
-        'alignright',
-        'alignleft',
-        'wp-caption',
-        'wp-caption-text',
-        'screen-reader-text',
-        'comment-list',
-    ],
-    whitelistPatterns: [
-        /^rtl(-.*)?$/,
-        /^home(-.*)?$/,
-        /^blog(-.*)?$/,
-        /^archive(-.*)?$/,
-        /^date(-.*)?$/,
-        /^error404(-.*)?$/,
-        /^logged-in(-.*)?$/,
-        /^admin-bar(-.*)?$/,
-        /^no-customize-support(-.*)?$/,
-        /^search(-.*)?$/,
-        /^nav(-.*)?$/,
-        /^wp(-.*)?$/,
-        /^wp-block-(a-z)?$/,
-        /^wp-custom-logo(-.*)?$/,
-        /^screen(-.*)?$/,
-        /^navigation(-.*)?$/,
-        /^(.*)-template(-.*)?$/,
-        /^(.*)?-?single(-.*)?$/,
-        /^postid-(.*)?$/,
-        /^post-(.*)?$/,
-        /^attachmentid-(.*)?$/,
-        /^attachment(-.*)?$/,
-        /^page(-.*)?$/,
-        /^(post-type-)?archive(-.*)?$/,
-        /^author(-.*)?$/,
-        /^gallery(-.*)?$/,
-        /^category(-.*)?$/,
-        /^tag(-.*)?$/,
-        /^card(-.*)?$/,
-        /^menu(-.*)?$/,
-        /^tags(-.*)?$/,
-        /^tax-(.*)?$/,
-        /^term-(.*)?$/,
-        /^date-(.*)?$/,
-        /^(.*)?-?paged(-.*)?$/,
-        /^says(-.*)?$/,
-        /^depth(-.*)?$/,
-        /^comment(-.*)?$/,
-        /^comments(-.*)?$/,
-        /^children(-.*)?$/,
-        /^crnb(-.*)?$/,
-        /^custom(-.*)?$/,
-        /^custom-background(-.*)?$/,
-        /^port-description(-.*)?$/,
-        /^gform_(.*)?$/,
-        /^gfield_(.*)?$/,
-        /^validation_(.*)?$/
-    ],
-}
-
 mix
     .setPublicPath('./assets')
     .sass('src/scss/base.scss', 'css').options(scssOptions)
     // Extract libraries requires ECMAScript 6 imports in your code.
     .js(bundles.all, 'js/app.js')
     .extract(extractLibs)
-
-    .purgeCss(
-        {
-            enabled: mix.inProduction(),
-            paths: [
-                /*
-                @todo: load parent theme templates
-                This might be an issue as a lot of the templates are
-                in the parent theme
-                 */
-                path.join(__dirname, '*.php'),
-                path.join(__dirname, 'templates/**/*.php'),
-                path.join(__dirname, '/assets/js/**/*.js'),
-            ],
-            extensions: ['html', 'js', 'php'],
-
-            // Other options are passed through to Purgecss
-            whitelist: purgecssWordpress.whitelist,
-            whitelistPatterns: purgecssWordpress.whitelistPatterns,
-        }
-    )
 
     .autoload({
         'jquery': ['$', 'window.jQuery', 'jQuery']
